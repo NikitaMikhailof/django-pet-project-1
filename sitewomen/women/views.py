167953244990985384cf5425cb4fff12de6b61ab
Nikita Mikhailov <mikhailoffnikita2016@yandex.ru>
2024-06-17 15:30:04 +0300

    third lesson

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
+from .models import Women
+
 
 menu = [{'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Добавить статью', 'url_name': 'add_page'},
@@ -24,29 +26,43 @@ cats_db = [
 ]
 
 def index(request):
+    posts = Women.published.all()
     data = {
             'title': 'Главная страница сайта',
             'menu': menu,
-            'posts':data_db,
+            'posts':posts,
             'cat_selected':0,
             
     }
     return render(request, 'women/index.html', context=data)
 
+
 def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с id = {post_id}')
+
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1, 
+    }
+    return render(request, 'women/post.html', data)
+
 
 def addpage(request):
     return HttpResponse('Добавление статьи')
 
+
 def contact(request):
     return HttpResponse('Добавление контактa')
 
+
 def login(request):
     return HttpResponse('Авторизация')
+
 
 def show_category(request, cat_id):
     data = {
@@ -56,6 +72,7 @@ def show_category(request, cat_id):
             'cat_selected':cat_id,      
     }
     return render(request, 'women/index.html', context=data)
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не определена<h1>')    
