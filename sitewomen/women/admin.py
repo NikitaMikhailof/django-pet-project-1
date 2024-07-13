0dbb0f137cf65b6b6f66e44a1371cf584c3e38e4
Nikita Mikhailov <mikhailoffnikita2016@yandex.ru>
2024-07-13 10:14:40 +0300

    sixth lesson

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,3 +1,58 @@
-from django.contrib import admin
+from typing import Any
+from django.contrib import admin, messages
+from django.db.models.query import QuerySet, Q
+from .models import Women, Category
 
-# Register your models here.
+
+admin.site.site_header = 'Панель администрирвоания'
+
+
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщины'
+    parameter_name = 'status'
+
+    def lookups(self, request: Any, model_admin: Any) -> list[tuple[Any, str]]:
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужеи')
+        ]
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    fields = ['title', 'slug', 'content', 'cat', 'husband', 'tags']
+    prepopulated_fields = {'slug': ('title',)}
+    # filter_horizontal = ['tags']
+    list_display = ('id', 'title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title',)
+    list_editable = ('is_published', )
+    ordering = ('-time_create', 'title')
+    actions = ['set_published', 'set_draft']
+    list_per_page = 5
+    search_fields = ['title', 'cat__name']
+    list_filter = [MarriedFilter, 'cat__name', 'is_published']
+
+    @admin.display(description='Краткое описание', ordering='content')
+    def brief_info (self, women:Women):
+        return f'Описание {len(women.content)} символов'
+    
+    @admin.action(description='Опубликовать')
+    def set_published(self, request, queryset):
+       count =  queryset.update(is_published=Women.Status.PUBLISHED)
+       self.message_user(request, f"Изменено {count} записи(ей).")
+
+    @admin.action(description='Снять с публикации')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f"{count} записи(ей) сняты с публикации!", messages.WARNING)
+
+
+@admin.register(Category)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+   
--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from django.utils.text import slugify
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -8,8 +8,12 @@ class PublishedManager(models.Manager):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='название')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='слаг')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
@@ -17,33 +21,42 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})
 
+
 class Women(models.Model):
     class Status(models.IntegerChoices):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
 
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
-    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='wumen')
+    title = models.CharField(max_length=255, verbose_name='название')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='слаг')
+    content = models.TextField(blank=True, verbose_name='описание')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                       default=Status.DRAFT, verbose_name="статус")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name='категория')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name='теги')
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='wumen', verbose_name='муж')
 
     objects = models.Manager()
     published = PublishedManager()
     
     class Meta:
+        verbose_name = 'Известные женщины мира'
+        verbose_name_plural = 'Известные женщины мира'
         ordering = ['-time_create']
         indexes = [
             models.Index(fields=['-time_create'])
         ]
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_slug': self.slug})        
+        return reverse('post', kwargs={'post_slug': self.slug}) 
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title, allow_unicode=True)
+    #     super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
